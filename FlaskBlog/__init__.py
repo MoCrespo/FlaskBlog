f380a53f579bdf4f99b4b7fe13b2b38381f42d32
MoCrespo <mcrespo495@gmail.com>
2021-05-15 09:53:16 +0200

    mail settings

--- FlaskBlog/__init__.py
+++ FlaskBlog/__init__.py
@@ -1,8 +1,9 @@
-
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd8074ce9dc96f9438a4129ddfab8cebb'
@@ -12,6 +13,16 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_USE_TLS'] = True
+
+
+mail = Mail(app)
+
+
 
 
 
